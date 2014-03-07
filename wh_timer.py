c7782bece3640ee47abd068657b8288b8b99d833
Mike Lopez <dev@scidentify.info>
2014-03-06 21:48:41 -0500

    basic sqlite connection for testing

--- wh_timer.py
+++ wh_timer.py
@@ -6,10 +6,34 @@ class Database(object):
 	"""
 	Database class handling connection and sql queries
 	"""
+	result = None
+	connection = False
+
 	def __init__(self):
+		"""Connect if kwargs present"""
 		if kwargs:
 			# try to connect
-			try:
-				con = sql.connect(DB_FILE)
-			except sql.Error, e:
-				sys.exit(1)
+			self.connect(*args, **kwargs)
+			
+
+	def connect(self, *args, **kwargs):
+		try:
+			connection = sql.connect(DB_FILE)
+			
+		except sql.Error, e:
+			raise Exception("Error connecting: %s" % e)
+			sys.exit(1)
+
+			
+	def query(self, query):
+		"""Return the query result."""
+		cur = self.connection.cursor()
+		cur.execute(query)
+		data = cur.fetchone()
+		return data
+
+
+	def close(self):
+		"""Closes the connection."""
+		if getattr(self, 'con'):
+			getattr(self, 'con').close()
