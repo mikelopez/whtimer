722bd1f99ed30b1261c8bc40bafd040f274a19fa
Mike Lopez <dev@scidentify.info>
2014-03-09 00:37:02 -0500

    completed logic test

--- tests/base.py
+++ tests/base.py
@@ -8,6 +8,8 @@ from datetime import datetime, timedelta
 from random import randint
 from db import Database
 
+MAX_ADD = 10
+
 class BaseTest(unittest.TestCase):
 
     def logi(self, data):
@@ -41,12 +43,18 @@ class BaseTest(unittest.TestCase):
         termprint("INFO", "DONE")
         db.close()
 
+    def query(self, data):
+        db = Database(connect=True)
+        q = db.query(data)
+        db.close()
+        return q
+
 
     def add_task(self, id, name, user):
         """Adds a task to Task table. Requires ID, NAME, USER params"""
         db = Database(connect=True)
         #print "INSERT INTO Task VALUES(%s, '%s', '%s');" % (id, name, user)
-        q = db.query("INSERT INTO Task VALUES(%s, '%s', '%s');" % (id, name, user))
+        q = db.query("insert into Task values(%s, '%s', '%s');" % (id, name, user))
         db.close() 
         return q
 
@@ -56,7 +64,7 @@ class BaseTest(unittest.TestCase):
         values = "('%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (id, task_id, user, 
                                                                  is_started, start, 
                                                                  stop, minutes_sum)
-        q = db.query("INSERT INTO TaskEvent VALUES%s;" % (values))
+        q = db.query("insert into TaskEvent values%s;" % (values))
         db.close() 
         return q
 
@@ -65,3 +73,49 @@ class BaseTest(unittest.TestCase):
     	q = db.select(query)
     	db.close()
     	return q
+
+    def get_count(self, col, tbl):
+        """Returns the count integer for a query"""
+        query = self.select('select count(%s) from %s' % (col, tbl))
+        try:
+            return query[0][0]
+        except IndexError:
+            return int(0)
+
+    def get_count_where(self, col, tbl, wherecol, whereval):
+        """Returns the count integer for a query"""
+        query = self.select("select count(%s) from %s where %s = '%s'" % (col, tbl, wherecol, whereval))
+        try:
+            return query[0][0]
+        except IndexError:
+            return int(0)
+
+
+
+    def check_database_counts(self):
+        """
+        Checks the counts in the databases to make
+        sure all test data was added with the right amounts.
+        Finally, creates the official test users task/taskevent
+        """
+        # test select 100 tasks / taskevents from setUp()
+        q = self.get_count('task_id', 'Task')
+        self.assertTrue(q == MAX_ADD)
+        q = self.get_count('id', 'TaskEvent')
+        self.assertTrue(q == MAX_ADD)
+
+        # checking tasks
+        query = self.select('select * from Task where task_id = 10')
+        self.assertTrue(len(query) == 1)
+
+        # check all tasks are stopped
+        q = self.get_count_where('id', 'TaskEvent', 'is_started', 0)
+        query = self.select('select count(id) from TaskEvent where is_started = 0')
+        self.assertTrue(query[0][0] == MAX_ADD)
+
+        # check task does not exist
+        query = self.select('select count(task_id) from Task where task_id = 420 and user = "Name 420"')
+        self.assertTrue(query[0][0] == 0)
+
+        # now add the task
+        new_task = self.add_task(420, 'MANTASK', 'Name 420')
--- tests/test_logic.py
+++ tests/test_logic.py
@@ -2,7 +2,6 @@
 
 from base import *
 
-MAX_ADD = 10
 
 class TestLogic(BaseTest):
     """
@@ -12,7 +11,7 @@ class TestLogic(BaseTest):
         """First create 100 task tests"""
         self.create_tables(dbfile="testdb.logic.db")
         self.logw("** Adding %s Test Tasks" % MAX_ADD)
-        for n in range(1, (MAX_ADD+1)):
+        for n in range(1, (MAX_ADD + 1)):
             randstr = str(randint(111, 999))
             self.add_task(n, "Task %s" % randstr, "Name %s" % randstr)
             started = (datetime.now() - timedelta(hours=3))
@@ -22,46 +21,16 @@ class TestLogic(BaseTest):
                                 started, ended, diff)
                                 
         
-        
+       
     def tearDown(self):
         termprint("WARNING", "Exiting....Deleting Database")
         #os.system('rm test.db')
 
 
-    def __check_database_counts(self):
-        """
-        Checks the counts in teh databases to make
-        sure all test data was added with the right amounts.
-        Finally, creates the official test users task/taskevent
-        """
-        # test select 100 tasks / taskevents from setUp()
-        query = self.select('select count(task_id) from Task')
-        self.loge("Returned %s" % query)
-        self.assertTrue(query[0][0] == MAX_ADD)
-        query = self.select('select count(id) from TaskEvent')
-        self.assertTrue(query[0][0] == MAX_ADD)
-
-        # checking tasks
-        query = self.select('select * from Task where task_id = 10')
-        self.assertTrue(len(query) == 1)
-
-        # check all tasks are stopped
-        query = self.select('select count(id) from TaskEvent where is_started = 0')
-        self.assertTrue(query[0][0] == MAX_ADD)
-
-        # check task does not exist
-        query = self.select('select count(task_id) from Task where task_id = 420 and user = "Name 420"')
-        self.assertTrue(query[0][0] == 0)
-
-        # now add the task
-        new_task = self.add_task(420, 'MANTASK', 'Name 420')
-
-
-
-    def __check_task_exists(self):
+    def __check_task_exists(self, task_id, user):
         """Check if the task exists for the user so we can create or update.
         Could possibly avoid this by always adding the task event when adding a task"""
-        c = self.select('select count(id) from TaskEvent where task_id = 420 and user = "Name 420"')
+        c = self.select('select count(id) from TaskEvent where task_id=%s and user="%s"' % (task_id, user))
         try:
             if int(c[0][0]) > 0:
                 return True 
@@ -69,14 +38,52 @@ class TestLogic(BaseTest):
         except IndexError:
             return False
 
-    def __insert_taskevent(self):
+
+    def __insert_taskevent(self, startdate=None, enddate=None):
         """Inserts a brand new task event with minutes sum hardcoded to 0."""
-        self.add_task_event(420, 420, "Name 420", 0, datetime.now(), None, 0)
+        self.add_task_event(420, 420, "Name 420", 0, startdate, enddate, 0)
 
 
-    def __check_task_running(self):
+    def __stopall(self, user):
+        """Stops all the processes.
+        TODO: Need to check if this takes longer than just doing a count(id) and running 
+        the update only if the count returns something larger than 0
+        """
+        for row in self.select('select * from TaskEvent where user = "%s"' % user):
+            rowd = {'is_started': row[3], 'sum_sec': int(row[6]), 'start': row[4],
+                    'task_id': row[1], 'user': row[2]}
+            if rowd.get('is_started') == int(1):
+                if start:
+                    sumsec += (datetime.now() - rowd.get('start')).seconds
+                    self.__stop_task(rowd.get('start'), rowd.get('task_id'), rowd.get('user'))
+                else:
+                    # something is wrong here, should reupdate 
+                    self.__stop_task(datetime.now(), rowd.get('task_id'), rowd.get('user'))
+                    pass
+
+
+    def __stop_task(self, start_date, task_id, user):
+        """Stops a task for a user, and updates the minutes sum"""
+        minutes_sum = (datetime.now() - start_date).seconds
+        q = 'update TaskEvent set is_started=0, stop="%s", minutes_sum=(minutes_sum + %s) ' % (
+            datetime.now(), minutes_sum)
+
+        q += 'where task_id=%s and user="%s"' % (task_id, user)
+        self.query(q)
+
+
+    def __start_task(self, task_id, user, startdate=datetime.now()):
+        """Starts the task for a user."""
+        self.__stopall(user)
+        self.query('update TaskEvent set is_started=1, start="%s" where task_id=%s and user="%s"' %\
+                   (startdate, task_id, user));
+
+
+    def __check_task_running(self, task_id, user):
         """Checks if the task is running if query result is > 0"""
-        c = self.select('select count(id) from TaskEvent where task_id = 420 and user = "Name 420" and is_started = 1')
+        query = 'select count(id) from TaskEvent where '
+        query += 'task_id=%s and user="%s" and is_started=1' % (task_id, user)
+        c = self.select(query)
         try:
             if c[0][0] > 0:
                 return True
@@ -86,31 +93,52 @@ class TestLogic(BaseTest):
 
 
     def test_logic(self):
-        # check the class
+        """
+        Tests the main logic.
+        Only one started task per user.
+        When starting a task, stop all tasks.
+        Update the minutes sum for each of the stopped tasks for a user
+        Set the requested task as started.
+        """
         from wh_timer import *
         wht = WHTimer()
 
-        # check database counts
-        self.__check_database_counts()
+        # check database counts and add a test task to work with (420)
+        self.check_database_counts()
 
         # user wants to start a task after creating it
         # first check if it exists
-        self.assertFalse(self.__check_task_exists())
-        self.assertFalse(self.__check_task_running())
-        # now add the tasks event
+        self.assertFalse(self.__check_task_exists(420, "Name 420"))
+        self.assertFalse(self.__check_task_running(420, "Name 420"))
+
+        # now add the tasks event as blank date since its the first add (stopped)
         self.__insert_taskevent()
-        self.assertTrue(self.__check_task_exists())
+        self.assertTrue(self.__check_task_exists(420, "Name 420"))
+
+        for i in range(0, 3):
+            started = (datetime.now() - timedelta(minutes=10))
+            self.__start_task(420, "Name 420", startdate=started)
+            self.assertTrue(self.__check_task_running(420, "Name 420"))
+            # stop it now 10 min later
+            self.__stop_task(started, 420, "Name 420")
+            self.assertFalse(self.__check_task_running(420, "Name 420"))
+  
+        q = self.select('select minutes_sum from TaskEvent where user = "%s" and task_id = "%s"' % ("Name 420", 420))
+        self.assertTrue(q[0][0] == int(30 * 60))
+        self.__print_results()
 
 
-        # now the user wants to start another task with something running! :)
-
-        # now the user wants to stop a task
-
-        # now the user wants to start another task again.... wtf? 
-
-        pass
-
-
+    def __print_results(self):
+        """Print an ugly table on terminal.....yeah!"""
+        results = self.select('select * from TaskEvent')
+        print "| ID\t| Task\t| User\t| Started\t| Start Date\t| End Date\t| Total Seconds"
+        print '-----------------------------------------------------------------------------------------------------------------'
+        for i in results:
+            row = ""
+            for col in i:
+                row += "| %s\t" % col
+            print row
+            print "-----------------------------------------------------------------------------------------------------------------"
 
 
 if __name__ == '__main__':
